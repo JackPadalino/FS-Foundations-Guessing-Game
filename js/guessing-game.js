9f8c1bdb0c14f17a7db6012ef0cf004eabd553ff
JackPadalino <pada0867@gmail.com>
2022-08-17 22:59:42 -0400

    updated CSS and refactored JS

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -14,27 +14,24 @@ class Game{
         };
     };
     checkGuess(guess){
+        let message='';
         if(this.pastGuesses.includes(this.playersGuess)){
-            return "You have already guessed that number.";
+            message="You have already guessed that number.";
         }else if(guess===this.winningNumber){
-            return 'You Win!';
+            message='You Win!';
         }else{
             this.pastGuesses.push(guess);
             if(this.pastGuesses.length===5){
-                return 'You Lose.';
+                message='You Lose.';
             }else{
                 const diff=this.difference();
-                if(diff<10){
-                    return "You're burning up!"
-                }else if(diff<25){
-                    return "You're lukewarm."
-                }else if(diff<50){
-                    return "You're a bit chilly."
-                }else{
-                    return "You're ice cold!"
-                }; 
+                if(diff<10) message="You're burning up!";
+                else if(diff<25) message="You're lukewarm.";
+                else if(diff<50) message="You're a bit chilly.";
+                else message="You're ice cold!";
             };
         };
+        return message;
     };
     difference(){
         return Math.abs(this.playersGuess-this.winningNumber);
@@ -73,46 +70,35 @@ function newGame(){
 // start new game
 function playGame(){
     let game=newGame();
-    
     // guess a number
-    let guessButton=document.getElementById('submit-guess-button');
+    const guessButton=document.getElementById('submit-guess-button');
     guessButton.addEventListener('click',function(){
         const guessForm=document.getElementById('enter-guess-form');
         const guess=Number(guessForm.value);
-        guessForm.value='';
-        let result=game.playersGuessSubmission(guess);
+        const result=game.playersGuessSubmission(guess);
         if(!(result==='You Win!')){
-            let incorrectGuessBox=document.getElementById(`incorrect-guess-box-${game.pastGuesses.length}`);
-            incorrectGuessBox.style.background='#FC8571';
-            let incorrectGuessHeading=document.getElementById(`incorrect-guess-${game.pastGuesses.length}`);
-            incorrectGuessHeading.innerHTML=guess;
+            document.getElementById(`incorrect-guess-box-${game.pastGuesses.length}`).style.background='#FC8571'; //update 'incorrect guess' boxes
+            document.getElementById(`incorrect-guess-${game.pastGuesses.length}`).innerHTML=guess;
         };
-        let messageText=document.getElementById('message');
-        messageText.innerHTML=result;
+        document.getElementById('message').innerHTML=result;
+        guessForm.value='';
     });
-
     // get a hint
-    let hintButton=document.getElementById('hint-button');
+    const hintButton=document.getElementById('hint-button');
     hintButton.addEventListener('click',function(){
         const hintArray=game.provideHint();
         const hintString=`The winning number is either ${hintArray[0]}, ${hintArray[1]}, or ${hintArray[2]}.`;
-        let messageText=document.getElementById('message');
-        messageText.innerHTML=hintString;
+        document.getElementById('message').innerHTML=hintString;
     });
-
     // reset the game
-    let playAgainButton=document.getElementById('play-again-button');
+    const playAgainButton=document.getElementById('play-again-button');
     playAgainButton.addEventListener('click',function(){
         game = newGame();
-        // reset message HTML
-        let messageText=document.getElementById('message');
-        messageText.innerHTML='Pick a number 1-100. You have five guesses!';
-        // reset 'incorrect guess' boxes background color
-        const incorrectGuessBoxes=document.getElementsByClassName('incorrect-guess-box');
+        document.getElementById('message').innerHTML='Pick a number 1-100. You have five guesses!'; // reset message heading
+        const incorrectGuessBoxes=document.getElementsByClassName('incorrect-guess-box');// reset 'incorrect guess' box background colors
         Array.prototype.forEach.call(incorrectGuessBoxes,box=>box.style.background='#98D7E6');
-        // reset 'incorrect guess' boxes HTML
-        const incorrectGuessHeadings=document.getElementsByClassName('incorrect-guess');
-        Array.prototype.forEach.call(incorrectGuessHeadings,box=>box.innerHTML='?');
+        const incorrectGuessHeadings=document.getElementsByClassName('incorrect-guess'); // reset 'incorrect guess' box headings
+        Array.prototype.forEach.call(incorrectGuessHeadings,heading=>heading.innerHTML='?');
     });
 // final closing bracket
 };
